0038f932ee300b1b178f9024a4dfa6d05ba697fc
James Scott Baronia <80136962+jsnb21@users.noreply.github.com>
2025-05-15 22:29:20 +0800

    Main Menu and Styling

--- src/game.js
+++ src/game.js
@@ -1,12 +1,21 @@
 import Phaser from 'phaser';
 import VNScene from '/src/scenes/vnScenes.js';
+import MainMenu from './scenes/mainmenu';
 
 const config = {
   type: Phaser.AUTO,
-  width: 800,
-  height: 600,
+  width: 816,
+  height: 624,
   parent: 'game',
-  scene: [VNScene]
+  scene: [MainMenu, VNScene]
+};
+
+export const DEFAULT_TEXT_STYLE = {
+    fontSize: '32px',
+    fontFamily: 'Jersey15-Regular, Arial, sans-serif', // Must match CSS
+    color: '#ffffff',
+    stroke: '#000000',        // Solid black outline
+    strokeThickness: 10
 };
 
 new Phaser.Game(config);
---
+++ src/scenes/mainmenu.js
@@ -0,0 +1,51 @@
+import Phaser from 'phaser';
+import { DEFAULT_TEXT_STYLE } from '../game';
+
+export default class MainMenu extends Phaser.Scene {
+    constructor() {
+        super('MainMenu');
+    }
+
+    preload() {
+        // Load your background image
+        this.load.image('menuBg', 'assets/img/mainmenu/SCI-HIGH_Title.png');
+    }
+
+    create() {
+        const { width, height } = this.scale;
+
+        // Add background image (centered and stretched to fit)
+        const bg = this.add.image(width / 2, height / 2, 'menuBg');
+        bg.setDisplaySize(width, height);
+
+        // Play Game Button
+        const playButton = this.add.text(width / 2, (height / 2) + 60, 'Play Game', {
+            ...DEFAULT_TEXT_STYLE,
+            color: '#ffff00'
+        }).setOrigin(0.5).setInteractive();
+
+        playButton.on('pointerdown', () => {
+            this.scene.start('VNScene');
+        });
+
+        // Continue Button
+        const continueButton = this.add.text(width / 2, height / 2 + 120, 'Continue', {
+            ...DEFAULT_TEXT_STYLE,
+            color: '#ffff00'
+        }).setOrigin(0.5).setInteractive();
+
+        continueButton.on('pointerdown', () => {
+            // Add your continue logic here
+        });
+
+        // Options Button
+        const optionsButton = this.add.text(width / 2, height / 2 + 180, 'Options', {
+            ...DEFAULT_TEXT_STYLE,
+            color: '#ffff00'
+        }).setOrigin(0.5).setInteractive();
+
+        optionsButton.on('pointerdown', () => {
+            // Add your options logic here
+        });
+    }
+}
