aeb6daebaceeecd85395c6f330ff1501da9dbd31
Xirnus <Xernous343@gmail.com>
2025-07-25 20:04:22 +0800

    Add build configuration and import fixing script

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,15 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   base: '/SCI-HIGH_THESIS/',
-  root: './docs',
   build: {
-    outDir: '../dist',
+    outDir: './dist',
     emptyOutDir: true,
+    rollupOptions: {
+      input: [
+        './docs/index.html',
+        './docs/game.html',
+        './docs/leaderboards.html'
+      ],
+    },
   },
 });
