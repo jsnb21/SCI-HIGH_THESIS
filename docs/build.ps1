ba4ce97901b9d5b6942a5405040afd13ecbf3c92
Xirnus <Xernous343@gmail.com>
2025-09-17 15:11:34 +0800

    Remove Files
    - Hope did not break

--- docs/build.ps1
+++
@@ -1,4 +0,0 @@
-# Build script for the reorganized project
-Write-Host "Building project..." -ForegroundColor Green
-Set-Location "config"
-npm run build
--- docs/run-dev.ps1
+++
@@ -1,4 +0,0 @@
-# Development server script for the reorganized project
-Write-Host "Starting development server..." -ForegroundColor Green
-Set-Location "config"
-npm run dev
